1ee00d66ec9053b51bcbcb1d567b01227a330f49
Arturo Aguilar <artrune@hotmail.com>
2021-11-30 12:10:41 -0600

    Synonym api for english and spanish words

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3.7
+
+RUN pip3 install fastapi uvicorn nltk
+COPY install_dictionary.py /install_dictionary.py
+RUN python3 /install_dictionary.py
+EXPOSE 8091
+
+COPY ./app /app
+
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8091"]
---
+++ app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import uvicorn
+from nltk.corpus import wordnet
+
+app = FastAPI(docs_url="/")
+
+@app.get("/spa")
+async def spa_synonym(word:str):
+    try:
+        results = []
+        for syn in wordnet.synsets(word, lang=('spa')):
+            for name in syn.lemma_names('spa'):
+                results.append(name)
+        return results
+        pass
+    except:
+        return []
+
+@app.get("/en")
+async def en_synonym(word:str):
+    try:
+        results = []
+        for syn in wordnet.synsets(word):
+            for name in syn.lemma_names():
+                results.append(name)
+        return results
+        pass
+    except:
+        return []
+
+if __name__=="__main__":
+    uvicorn.run("main:app",host='localhost', port=8091, reload=True, debug=True)
